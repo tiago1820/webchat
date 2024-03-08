5a9f3b5d36643068caed65b851c239981536d2f0
tiago1820 <tiago.zdo@gmail.com>
2024-03-08 12:41:43 -0300

    Styling

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ import { ChatCard } from '../ChatCard/ChatCard';
 import { useState } from 'react';
 import { MessageCard } from '../MessageCard/MessageCard';
 import { ImAttachment } from 'react-icons/im';
-
+import './HomePage.css';
 
 export const HomePage = () => {
     const [querys, setQuerys] = useState(null);
@@ -58,8 +58,8 @@ export const HomePage = () => {
                                 <BsFilter className='ml-4 text-3xl' />
                             </div>
                         </div>
-                        {/* Todos los usuarios */}
-                        <div className='bg-white overflow-x-scroll h-[76.8vh] px-3'>
+                        {/* all user */}
+                        <div className='bg-white overflow-y-scroll h-[72vh] px-3'>
                             {querys &&
                                 [1, 1, 1, 1, 1].map((item) => (
                                     <div onClick={handleClickOnChatCard}>
@@ -82,55 +82,57 @@ export const HomePage = () => {
                     </div>
                 </div>}
                 {/* message part */}
-                {currentChat && <div className='w-[70%] relative'>
-                    <div className='header absolute top 0 w full bg-[#f0f2f5'>
-                        <div className='flex justify-between'>
-                            <div className='py-3 space-x-4 flex items-center px-3'>
-                                <img className='w-10 h-10 rounded-full' src="https://cdn.pixabay.com/photo/2023/04/13/09/56/alps-7922246_1280.jpg" alt="" />
-                                <p>
-                                    username
-                                </p>
-                            </div>
-                            <div className='py-3 flex space-x-4 items-center px-3'>
-                                <AiOutlineSearch />
-                                <BsThreeDotsVertical />
+                {currentChat && (
+                    <div className='w-[70%] relative bg-blue-200'>
+                        <div className='header absolute top 0 w full bg-[#f0f2f5'>
+                            <div className='flex justify-between'>
+                                <div className='py-3 space-x-4 flex items-center px-3'>
+                                    <img className='w-10 h-10 rounded-full' src="https://cdn.pixabay.com/photo/2023/04/13/09/56/alps-7922246_1280.jpg" alt="" />
+                                    <p>
+                                        username
+                                    </p>
+                                </div>
+                                <div className='py-3 flex space-x-4 items-center px-3'>
+                                    <AiOutlineSearch />
+                                    <BsThreeDotsVertical />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    {/* message section */}
-                    <div className='px-10 h-[85vh] overflow-y-scroll bg-blue-200'>
-                        <div className='space-y-1 flex flex-col justify-center mt-20 py-2'>
-                            {[1, 1, 1, 1, 1,].map((item, i) => <MessageCard isReqUserMessage={i % 2 === 0} content={"message"} />)}
+                        {/* message section */}
+                        <div className='px-10 h-[85vh] overflow-y-scroll'>
+                            <div className='space-y-1 flex flex-col justify-center mt-20 py-2'>
+                                {[1, 1, 1, 1, 1,].map((item, i) => <MessageCard isReqUserMessage={i % 2 === 0} content={"message"} />)}
+                            </div>
                         </div>
-                    </div>
-                    {/* footer part */}
-                    <div className='footer bg-[#f0f2f5] absolute bottom-0 w-full py-3 text-2xl'>
-                        <div className='flex justify-between items-center px-5 relative'>
+                        {/* footer part */}
+                        <div className='footer bg-[#f0f2f5] absolute bottom-0 w-full py-3 text-2xl'>
+                            <div className='flex justify-between items-center px-5 relative'>
                                 <BsEmojiSmile className='cursor-pointer' />
                                 <ImAttachment />
 
-                            <input
-                                className='py-2 outline-none border-none bg-white pl-4 rounded-md w-[85%]'
-                                type="text"
-                                onChange={(e) => setContent(e.target.value)}
-                                placeholder='Type message'
-                                value={content}
-                                onKeyPress={(e) => {
-                                    if (e.key === "Enter") {
-                                        handleCreateNewMessage();
-                                        setContent("");
-                                    }
-                                }}
-                            />
-                            <BsMicFill />
+                                <input
+                                    className='py-2 outline-none border-none bg-white pl-4 rounded-md w-[85%]'
+                                    type="text"
+                                    onChange={(e) => setContent(e.target.value)}
+                                    placeholder='Type message'
+                                    value={content}
+                                    onKeyPress={(e) => {
+                                        if (e.key === "Enter") {
+                                            handleCreateNewMessage();
+                                            setContent("");
+                                        }
+                                    }}
+                                />
+                                <BsMicFill />
+
+                            </div>
+
+
 
                         </div>
 
-
-
                     </div>
-
-                </div>}
+                )}
 
             </div>
         </div>
